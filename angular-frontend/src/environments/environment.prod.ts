31e0cdd2d9bf323109cedd3fdcc13eca86c6ca35
Priyail <nidhipandey8108@gmail.com>
2026-04-03 16:39:12 +0530

    update

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://127.0.0.1:5000/api'
+  apiBaseUrl: 'http://127.0.0.1:5000/api, https://ai-healthcare-assistant-1-ok54.onrender.com/api'
 };
